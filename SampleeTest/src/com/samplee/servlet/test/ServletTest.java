0ffade226a085b4c0de4cdc1e402dfe50fe9c42f
Rangel Ivanov <Iron_steel_88@abv.bg>
2011-08-04 12:38:50 +0300

    Test that the servlet returns SampleObject

--- SampleeTest/src/com/samplee/servlet/test/ServletTest.java
+++ SampleeTest/src/com/samplee/servlet/test/ServletTest.java
@@ -2,6 +2,8 @@ package com.samplee.servlet.test;
 
 import static org.junit.Assert.*;
 
+import javax.xml.ws.WebEndpoint;
+
 import com.samplee.*;
 
 import com.sun.jersey.api.client.*;
@@ -19,5 +21,20 @@ public class ServletTest {
 
 		// We are expecting a string containing "hello from servlet"
 		assertEquals("hello from servlet", webResource.get(String.class));
+	}
+	
+	@Test 
+	public void testServletReturnsSampleModel() {
+		Client client = Client.create();
+
+		// Fetch resource form servlet
+		WebResource webResource = client
+				.resource("http://localhost:8080/SampleeWeb/API/samplemodel");
+		SampleModel result = webResource.get(SampleModel.class);
+		// Do we get the expected object
+		assertEquals(SampleModel.class, result.getClass());
+		
+		// And does the name field contains the correct string value
+		assertEquals("Hello from json", result.getName());
 	}
 }
